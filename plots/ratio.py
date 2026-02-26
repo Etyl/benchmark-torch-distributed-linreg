339dad1c4e3911c570525cc69d0c055e2be5afa7
Hippolyte Verninas <etyl.sani@gmail.com>
2026-02-26 16:09:54 +0100

    n_reps

--- plots/ratio.py
+++ plots/ratio.py
@@ -12,22 +12,21 @@ class Plot(BasePlot):
         df = df[df["dataset_name"].str.contains(dataset, na=False)]
 
         plots = []
-        for k in range(len(df)):
-            solver= df["solver_name"].iloc[k]
-            y = df["objective_comm_ratio"].iloc[k]
-            solver_name = solver.split("[")[0]
-            batch_size = solver.split("batch_size=")[1].split(",")[0]
-            d1 = df["dataset_name"].iloc[k]
-            d1 = d1.split("d1=")[1].split(",")[0]
-            solver_label = f"{solver_name}[batch_size={batch_size},d1={d1}]"
-            curve_data = {
-                "x": [int(batch_size) / int(d1)],
-                "y": [y],
-                "label": solver_label,
-                **self.get_style(solver_label)
-            }
+        for solver, df_solver in df.groupby("solver_name"):
+            for dataset_name, df_dataset in df_solver.groupby("dataset_name"):
+                y = df_dataset["objective_comm_ratio"].values.to_list()
+                solver_name = solver.split("[")[0]
+                batch_size = solver.split("batch_size=")[1].split(",")[0]
+                d1 = dataset_name.split("d1=")[1].split(",")[0]
+                solver_label = f"{solver_name}[batch_size={batch_size},d1={d1}]"
+                curve_data = {
+                    "x": [int(batch_size) / int(d1)] * len(y),
+                    "y": y,
+                    "label": solver_label,
+                    **self.get_style(solver_label)
+                }
 
-            plots.append(curve_data)
+                plots.append(curve_data)
 
         return plots
 
